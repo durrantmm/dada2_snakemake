26ea5b6bf0d9f12c7b3a3e1b321eda6441591854
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-05-19 08:03:35 -0600

    update

---
+++ scripts/error_model.R
@@ -0,0 +1,25 @@
+if (!("dada2" %in% installed.packages()[,"Package"])){
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("dada2")  
+}
+library(dada2); packageVersion("dada2")
+
+args = commandArgs(trailingOnly=TRUE)
+
+error_rdata <- args[1]
+files <- args[2:length(args)]
+FWD.fastqs <- files[1:(length(files)/2)]
+REV.fastqs <- files[((length(files)/2)+1):length(files)]
+
+dir.create(dirname(error_rdata))
+
+# Learn forward error rates
+print("Learning forward error rates from 2 million reads...")
+errF <- learnErrors(FWD.fastqs, nread=2e6, multithread=TRUE)
+
+# Learn reverse error rates
+print("Learning reverse error rates from 2 million reads...")
+errR <- learnErrors(filtRs, nread=2e6, multithread=TRUE)
+
+print("Saving error rates...")
+save(errF, errR, file=error_rdata)
