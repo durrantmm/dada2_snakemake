6db0178a6a6df4298c18d0d3c1527531d3733b53
M Durrant <mdurrant@DN52eg8n.SUNet>
2017-05-30 15:00:31 -0600

    update

--- scripts/assign_taxonomies_rdp.R
+++ scripts/assign_taxonomies_rdp.R
@@ -11,6 +11,7 @@ rdp_train_set <- args[2]
 rdp_species_train_set <- args[3]
 rdp_taxa_out_rds <- args[4]
 
+print("Assigning initial taxonomies using rdp training set...")
 seqtab <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE)
 
 print("Assigning initial taxonomies using rdp training set...")
---
+++ scripts/remove_bimeras.R
@@ -0,0 +1,21 @@
+if (!("dada2" %in% installed.packages()[,"Package"])){
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("dada2")  
+}
+library(dada2); packageVersion("dada2")
+library(readr); packageVersion("readr")
+args = commandArgs(trailingOnly=TRUE)
+
+seqtab <- readRDS(args[1], refhook = NULL)
+seqtab_out_rds <- args[2]
+seqtab_out_tsv <- args[3]
+
+print("Assigning initial taxonomies using rdp training set...")
+seqtab <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE)
+
+print("Writing to file...")
+saveRDS(seqtab, seqtab_out_rds) 
+
+seqtab_tsv <- cbind(data.frame(ID=rownames(data.frame(seqtab))), 
+                           data.frame(seqtab)) 
+write_tsv(seqtab_tsv, seqtab_out_tsv) 
